146221f8f81d8943e683570dd7c69875b72bfb7f
Alexey Mylnikov <alexey.mylnikov@playrix.com>
2019-02-20 21:46:25 +0300

    Добавлена логика работы с хранилищем и юнит-тесты

--- 03_oop/homework/app/api/store.py
+++ 03_oop/homework/app/api/store.py
@@ -1,13 +1,9 @@
 import time
 import redis
-from app.api.cache import Cache
-from app.api.exceptions import CacheKeyError
 
 
 class Store(object):
-    def __init__(self, cache_size, host, timeout=1, retry=3, backoff=0.3, **kwargs):
-        self.cache = Cache(cache_size)
-
+    def __init__(self, host, timeout=5, retry=3, backoff=0.3, **kwargs):
         kwargs['socket_timeout'] = timeout
         self.store = redis.Redis(host,  **kwargs)
         self.retry = retry
@@ -28,19 +24,11 @@ class Store(object):
         return wrapper
 
     def set(self, key, value, expire=None):
-        when = expire and int(time.time() + expire)
-
-        self.cache.append(key, value)
         self._retry_wrapper(self.store.append)(key, value)
-
-        if when:
-            self.cache.expireat(key, when)
-            self._retry_wrapper(self.store.expireat)(key, when)
+        if expire:
+            self._retry_wrapper(self.store.expireat)(key, int(time.time() + expire))
 
     def get(self, key):
-        try:
-            return self.cache.get(key)
-        except CacheKeyError:
             return self._retry_wrapper(self.store.get)(key)
 
     def cache_set(self, key, value, expire=None):
--- 03_oop/homework/tests/unit/api/test_store.py
+++ 03_oop/homework/tests/unit/api/test_store.py
@@ -1,66 +1,85 @@
 import unittest
+from mock import patch
 from app.api.store import Store
+from tests.mixins import AssertMixin
+from tests.mocks import MockRedis
+from tests.decorators import cases
+
+E = Exception
 
 
-def counter(success_on=1):
-    i = [0, success_on]
+class TestStore(AssertMixin, unittest.TestCase):
+    @patch('redis.Redis', **{'return_value.append.side_effect': [None, None, None]})
+    @cases([('a', 0, 1), ('b', 1, 2), ('c', -2, 3)])
+    def test_retry_wrapper_call_set_one_time_if_exception_not_raises(self, mock_redis, key, value, retry):
+        with self.assertNotRaises(E):
+            Store('mocked', retry=retry).set(key, value)
 
-    def incr():
-        i[0] = i[0] + 1
-        assert i[0] == i[1], 'Fake exception'
-        return i[0]
+    @patch('redis.Redis', **{'return_value.append.side_effect': [E(), None, E(), E(), None, E(), E(), E(), None]})
+    @cases([('a', 0, 1), ('b', 1, 2), ('c', -2, 3)])
+    def test_retry_wrapper_call_set_n_times_if_exception_raises(self, mock_redis, key, value, retry):
+        with self.assertNotRaises(E):
+            Store('mocked', retry=retry).set(key, value)
 
-    return incr
+    @patch('redis.Redis', **{'return_value.get.side_effect': [None, None, None]})
+    @cases([('a', 1), ('b', 2), ('c', 3)])
+    def test_retry_wrapper_call_get_one_time_if_exception_not_raises(self, mock_redis, key, retry):
+        with self.assertNotRaises(E):
+            Store('mocked', retry=retry).get(key)
 
+    @patch('redis.Redis', **{'return_value.get.side_effect': [E(), None, E(), E(), None, E(), E(), E(), None]})
+    @cases([('a', 0, 1), ('b', 1, 2), ('c', -2, 3)])
+    def test_retry_wrapper_call_get_n_times_if_exception_raises(self, mock_redis, key, value, retry):
+        with self.assertNotRaises(E):
+            Store('mocked', retry=retry).set(key, value)
 
-class TestStore(unittest.TestCase):
-    def test_retry_wrapper_call_func_one_time_if_exception_not_raises(self):
-        func = counter()
-        wrapped = Store('10', 'localhost')._retry_wrapper(func)
-        self.assertEqual(1, wrapped(), 1)
+    @patch('redis.Redis', **{'return_value': MockRedis()})
+    @cases([('a', 0), ('b', 1, 10), ('c', -2), ('d', 3.3, 20), ('e', -4.4), ('f', 2 ** 32, 30)])
+    def test_set_success_if_store_available(self, mock_redis, key, value, expired=None):
+        with self.assertNotRaises(E):
+            Store('mocked').set(key, value, expired)
 
-    def test_retry_wrapper_call_func_n_times_if_exception_raises(self):
-        retry = 3
-        func = counter(success_on=3)
-        wrapped = Store('10', 'localhost', retry=retry)._retry_wrapper(func)
+    @patch('redis.Redis', **{'return_value.append.side_effect': E()})
+    @cases([('a', 0), ('b', 1, 10), ('c', -2), ('d', 3.3, 20), ('e', -4.4), ('f', 2 ** 32, 30)])
+    def test_set_raises_if_store_unavailable(self, mock_redis, key, value, expired=None):
+        with self.assertRaises(E):
+            Store('mocked').set(key, value, expired)
 
-        self.assertEqual(retry, wrapped(), retry)
+    @patch('redis.Redis', **{'return_value': MockRedis()})
+    @cases(['a', 'b', 'c', 'd', 'e', 'f'])
+    def test_get_success_if_store_available(self, mock_redis, key):
+        with self.assertNotRaises(E):
+            Store('mocked').get(key)
 
-    def test_set_value_success_if_store_available_and_key_unique(self):
-        pass
+    @patch('redis.Redis', **{'return_value.get.side_effect': E()})
+    @cases(['a', 'b', 'c', 'd', 'e', 'f'])
+    def test_get_raises_if_store_unavailable(self, mock_redis, key):
+        with self.assertRaises(E):
+            Store('mocked').get(key)
 
-    def test_set_value_success_if_store_available_and_key_not_unique(self):
-        pass
+    @patch('redis.Redis', **{'return_value': MockRedis()})
+    @cases([('a', 0), ('b', 1, 10), ('c', -2), ('d', 3.3, 20), ('e', -4.4), ('f', 2 ** 32, 30)])
+    def test_cache_set_success_if_store_available(self, mock_redis, key, value, expired=None):
+        with self.assertNotRaises(E):
+            Store('mocked').cache_set(key, value, expired)
 
-    def test_set_value_raises_if_store_unavailable(self):
-        pass
+    @patch('redis.Redis', **{'return_value.append.side_effect': E()})
+    @cases([('a', 0), ('b', 1, 10), ('c', -2), ('d', 3.3, 20), ('e', -4.4), ('f', 2 ** 32, 30)])
+    def test_cache_set_success_if_store_unavailable(self, mock_redis, key, value, expired=None):
+        with self.assertNotRaises(E):
+            Store('mocked').cache_set(key, value, expired)
 
-    def test_get_value_success_if_store_available_and_key_exists(self):
-        pass
+    @patch('redis.Redis', **{'return_value': MockRedis()})
+    @cases(['a', 'b', 'c', 'd', 'e', 'f'])
+    def test_cache_get_success_if_store_available(self, mock_redis, key):
+        with self.assertNotRaises(E):
+            Store('mocked').cache_get(key)
 
-    def test_get_value_raises_if_store_available_and_key_not_exists(self):
-        pass
-
-    def test_get_value_raises_if_store_unavailable(self):
-        pass
-
-    def test_get_value_success_if_store_available_and_key_not_expired(self):
-        pass
-
-    def test_get_value_success_if_store_available_and_key_expired(self):
-        pass
-
-    def test_set_cache_value_success_if_store_available(self):
-        pass
-
-    def test_set_cache_value_success_if_store_unavailable(self):
-        pass
-
-    def test_get_cache_value_success_if_store_available(self):
-        pass
-
-    def test_get_cache_value_success_if_store_unavailable(self):
-        pass
+    @patch('redis.Redis', **{'return_value.append.side_effect': E()})
+    @cases(['a', 'b', 'c', 'd', 'e', 'f'])
+    def test_cache_get_success_if_store_unavailable(self, mock_redis, key):
+        with self.assertNotRaises(E):
+            Store('mocked').cache_get(key)
 
 
 if __name__ == "__main__":
